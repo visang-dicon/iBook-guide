f02c4255ea2fa75edb76a620f6f643c442d7e99c
visang-dicon <visangdesign01@gmail.com>
2026-05-13 17:32:22 +0900

    Create config.mjs

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,8 @@
+export default {
+  title: "iBook Studio 가이드",
+  base: '/iBook-guide/',
+  themeConfig: {
+    nav: [{ text: '홈', link: '/' }, { text: '가이드', link: '/guide' }],
+    sidebar: [{ text: '시작하기', items: [{ text: '가이드 시작', link: '/guide' }] }]
+  }
+}
